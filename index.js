02d61a2cbd657c4a543ed744948bc2c791bf2a27
parwejalam <parwejalamgt@gmail.com>
2024-08-13 18:30:50 +0530

    Created a excel to json program and it also convert those json data into a sql script file which help to insert data to database

---
+++ index.js
@@ -0,0 +1,62 @@
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+
+const vesselSheetName = 'Vessel Particlars ';
+
+const allData = excelToJson({
+    sourceFile: 'exceles/CAPE ELIANTO.xlsx',
+    header: {
+        rows:1 ,// Skip the first row (likely a header row)
+    },
+   
+    sheets:[
+        {
+            name: vesselSheetName,
+            range: 'A2:T2',
+            columnToKey: {
+                B: 'VesselName',
+                C: 'CallSign',
+                D: 'Flag',
+                E: 'PortOfRegistry',
+                F: 'OfficialNumber',
+                G: 'ImoNumber',
+                H: 'MMSI',
+                I: 'ClassSociety',
+                J: 'VesselType',
+                K: 'DateKeelLaid',
+                L: 'Delivery',
+                M: 'ClassNotation',
+                N: 'PIClub',  // Correct the column name to match your data structure
+                O: 'Owners',
+                P: 'Operators',
+                Q: 'LOA',
+                R: 'LBP',
+                S: 'BreadthMoulded',
+                T: 'DepthModulded'
+            }
+        }
+    ]
+});
+console.log(allData);
+/******* Prepare data *******/
+let vesselParticlars = allData[vesselSheetName];
+
+/***  Write Providers data ***********************/
+let vesselSqls = '';
+for (let index = 0; index < vesselParticlars.length; index++) {
+    const item = vesselParticlars[index];
+
+    if (item.VesselName) {
+        vesselSqls +=
+            `INSERT INTO public."VesselParticlars"(
+            "VesselName", "CallSign", "Flag", "PortOfRegistry", "OfficialNumber", "ImoNumber", "MMSI", "ClassSociety", "VesselType", "DateKeelLaid", "Delivery", "ClassNotation", "PIClub", "Owners", "Operators", "LOA", "LBP", "BreadthMoulded", "DepthModulded"
+            ) OVERRIDING SYSTEM VALUE VALUES (
+              '${item.VesselName}', '${item.CallSign}', '${item.Flag}', '${item.PortOfRegistry}', '${item.OfficialNumber}', '${item.ImoNumber}', '${item.MMSI}', '${item.ClassSociety}', '${item.VesselType}', '${item.DateKeelLaid}', '${item.Delivery}', '${item.ClassNotation}', '${item.PIClub}', '${item.Owners}', '${item.Operators}', '${item.LOA}', '${item.LBP}', '${item.BreadthMoulded}', '${item.DepthModulded}'
+            );\r\n`
+    }
+}
+// Write to vessel.sql
+fs.writeFile('sqls/vessel.sql', vesselSqls, function (err) {
+    if (err) throw err;
+    console.log('Vessel Particlars Saved!');
+});
---
+++ sqls/vessel.sql
@@ -0,0 +1,5 @@
+INSERT INTO public."VesselParticlars"(
+            "VesselName", "CallSign", "Flag", "PortOfRegistry", "OfficialNumber", "ImoNumber", "MMSI", "ClassSociety", "VesselType", "DateKeelLaid", "Delivery", "ClassNotation", "PIClub", "Owners", "Operators", "LOA", "LBP", "BreadthMoulded", "DepthModulded"
+            ) OVERRIDING SYSTEM VALUE VALUES (
+              'CAPE ELIANTO', '9V7479', 'Singapore', 'Singapore', '402539', '9938602', '563163200', 'NKK', 'Bulk Carrier', 'Wed Dec 16 2020 23:59:50 GMT+0530 (India Standard Time)', 'Sun Jul 24 2022 23:59:50 GMT+0530 (India Standard Time)', 'NS* (CSR, BC-A, BC-XII, GRAB 35, PSPC-WBT, NC, 1C)', 'Standard P&I Club per Charles Taylor & Co', 'STC SHIPPING PTE.LTD', 'Sealogger Test 2', '292', '288.4', '45', '24.55'
+            );
